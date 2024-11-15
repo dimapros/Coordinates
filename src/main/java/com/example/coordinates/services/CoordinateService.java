e5b57530ed85fc8be0ffe86bcce9d5d0a5e4f109
DmitryProskuryakov <proskurakov1998@gmail.com>
2024-11-15 10:26:56 +1000

    Внес правки.

--- src/main/java/com/example/coordinates/services/CoordinateService.java
+++ src/main/java/com/example/coordinates/services/CoordinateService.java
@@ -1,37 +1,11 @@
 package com.example.coordinates.services;
 
 import com.example.coordinates.entity.Coordinate;
-import com.example.coordinates.entity.NominatimResponse;
-import com.example.coordinates.utils.Util;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class CoordinateService {
-    private final RestTemplate restTemplate;
-    private static final String NOMINATIM_URL = "https://nominatim.openstreetmap.org";
+public interface CoordinateService {
+    Coordinate getAddressAndDistance(Coordinate coordinate);
 
-
-    public CoordinateService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    public Coordinate getAddressAndDistance(Coordinate coordinate) {
-        coordinate.setStartAddress(getAddress(coordinate.getStartPos()));
-        coordinate.setEndAddress(getAddress(coordinate.getEndPos()));
-        coordinate.setDistance(Util.getDistance(coordinate.getStartPos(), coordinate.getEndPos()));
-
-        return coordinate;
-    }
-
-    public String getAddress(List<Double> pos) {
-        String url = NOMINATIM_URL + "/reverse?lat=" +
-                pos.get(0) + "&lon=" + pos.get(1) + "&format=json";
-        Optional<NominatimResponse> forObject = Optional.ofNullable(restTemplate.getForObject(url, NominatimResponse.class));
-
-        return forObject.map(NominatimResponse::getAddress).orElse("Unknown address");
-    }
+    String getAddress(List<Double> pos);
 }
---
+++ src/main/java/com/example/coordinates/services/CoordinateServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.coordinates.services;
+
+import com.example.coordinates.entity.Coordinate;
+import com.example.coordinates.entity.NominatimResponse;
+import com.example.coordinates.utils.Util;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CoordinateServiceImpl implements CoordinateService{
+    private final RestTemplate restTemplate;
+    private static final String NOMINATIM_URL = "https://nominatim.openstreetmap.org";
+
+    public CoordinateServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Coordinate getAddressAndDistance(Coordinate coordinate) {
+        coordinate.setStartAddress(getAddress(coordinate.getStartPos()));
+        coordinate.setEndAddress(getAddress(coordinate.getEndPos()));
+        coordinate.setDistance(Util.getDistance(coordinate.getStartPos(), coordinate.getEndPos()));
+
+        return coordinate;
+    }
+
+    public String getAddress(List<Double> pos) {
+        String url = NOMINATIM_URL + "/reverse?lat=" +
+                pos.get(0) + "&lon=" + pos.get(1) + "&format=json";
+        Optional<NominatimResponse> forObject = Optional.ofNullable(restTemplate.getForObject(url, NominatimResponse.class));
+
+        return forObject.map(NominatimResponse::getAddress).orElse("Unknown address");
+    }
+}
